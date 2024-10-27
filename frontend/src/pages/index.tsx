7578b151c5e3796cccb33b8171f01e626a6bdf5f
Mucino11 <64760426+Mucino11@users.noreply.github.com>
2024-10-27 06:11:48 +0100

    .

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -48,7 +48,7 @@ export default function Home() {
             {projectsState.map((project) => (
               <article key={project.id} className="project-card">
                 <h2 className="project-h2">Name: {project.name}</h2>
-                <p>Descriptions: {project.description}</p>
+                <p>Description: {project.description}</p>
                 <p>Status: {project.status}</p>
                 <p>
                   Published on:{" "}
